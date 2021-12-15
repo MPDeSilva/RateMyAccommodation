2773f97f457751aee36816252dbf858244c84cbc
Milinda Prasan De Silva <milindapds@hotmail.com>
2021-12-15 22:37:50 +0000

    Laying Foundations

---
+++ ratemyaccommodation/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <div className="flex h-full w-full bg-blue-300">
+    <div className=" flex flex-col max-w-7xl mx-auto justify-center items-center">
+      <h1 className="text-7xl">Hello</h1>
+
+
+    </div>
+    </div>
+      
+    
+    
+  )
+}
