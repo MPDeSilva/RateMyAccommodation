9e9ba0166697bc20d6ae6a3e3c43fbdc105792cb
Milinda Prasan De Silva <milindapds@hotmail.com>
2021-12-29 16:20:57 +0000

    ud

--- ratemyaccommodation/pages/_app.tsx
+++ ratemyaccommodation/pages/_app.tsx
@@ -1,9 +1,16 @@
+import Head from 'next/head'
 import Footer from '../components/Footer'
 import NavBar from '../components/NavBar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <>
+  <Head>
+    <title>Rate My Accommodation</title>
+    <meta charSet="utf-8" />
+    <meta property='og:image' content="/Logo.png" />
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+  </Head>
   <div>
     <NavBar />
 
