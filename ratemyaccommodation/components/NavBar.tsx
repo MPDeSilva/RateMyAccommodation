70784331ad5d086449ebab93614df3902bdacd16
Milinda Prasan De Silva <milindapds@hotmail.com>
2021-12-30 15:40:35 +0000

    Test

--- ratemyaccommodation/components/NavBar.tsx
+++ ratemyaccommodation/components/NavBar.tsx
@@ -69,14 +69,14 @@ const NavBar = () => {
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <button
                   type="button"
-                  className="md:flex hidden bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                  className="md:flex hidden mr-0 bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                 >
                   <span className="sr-only">View notifications</span>
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
 
                 {/* Profile dropdown */}
-                <Menu as="div" className="ml-3 sm:right-10 right-6 relative">
+                <Menu as="div" className="ml-3 sm:right-10 right-6 md:right-0 relative">
                   <div>
                     <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                       <span className="sr-only">Open user menu</span>
