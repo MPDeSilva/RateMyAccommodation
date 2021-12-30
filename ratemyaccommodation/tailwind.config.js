d6bbb918596f05da52c56a415a6bf98994ac0179
Milinda Prasan De Silva <milindapds@hotmail.com>
2021-12-30 22:48:14 +0000

    test

--- ratemyaccommodation/tailwind.config.js
+++ ratemyaccommodation/tailwind.config.js
@@ -1,20 +1,5 @@
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    'postcss-flexbugs-fixes',
-    [
-      'postcss-preset-env',
-      {
-        autoprefixer: {
-          flexbox: 'no-2009',
-        },
-        stage: 3,
-        features: {
-          'custom-properties': false,
-        },
-      },
-    ],
-  ],
+  plugins: [],
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   media: false,
   important: true,
