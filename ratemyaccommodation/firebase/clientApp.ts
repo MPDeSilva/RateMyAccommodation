b72eceed2f4caff43411bcfed14b97cd88cbb47e
Milinda Prasan De Silva <milindapds@hotmail.com>
2021-12-30 21:09:37 +0000

    test

--- ratemyaccommodation/firebase/clientApp.ts
+++ ratemyaccommodation/firebase/clientApp.ts
@@ -2,9 +2,10 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/auth';
+import nextcon from '../next.config';
 
 const clientCredentials = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+  apiKey:  process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
